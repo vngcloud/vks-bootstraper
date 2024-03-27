54ab47d083c113d4cf258065bf3ab7076b191738
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-03-27 09:23:24 +0700

    This is Manh Cuong's commit 🌻.

--- vks_bootstraper/__main__.py
+++ vks_bootstraper/__main__.py
@@ -5,11 +5,10 @@ Description: The CLI tool to boostrap the instances of VKS clusters.
 
 import click
 from . import metadata
-from . import __version__
 
 
 @click.group()
-@click.version_option(__version__, prog_name="vks-bootstraper")
+@click.version_option("0.1.0", prog_name="vks-bootstraper")
 def cli():
     pass
 
